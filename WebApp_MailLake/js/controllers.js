0a709a3d052506b8deedb9907166c3181d10b410
gcrev93 <gcrev93@hotmail.com>
2015-10-23 11:47:39 -0700

    added angular support

---
+++ WebApp_MailLake/js/controllers.js
@@ -0,0 +1,7 @@
+﻿angular.module('mailLake', []).controller('bubController', function ($scope) {
+    $scope.firstName = "John";
+    $scope.lastName = "Doe";
+    $scope.fullName = function () {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+});
