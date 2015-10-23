121afdaccdb035ea53cb5a34450d4c15815174e9
Jesse van Leth <jevanlet@microsoft.com>
2015-10-23 17:49:28 +0200

    removed some functions/added a commented new one

---
+++ UnifiedApiConnect/Models/email.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnifiedApiConnect.Models
+{
+    public class Email
+    {
+        public string subject { get; set; }
+        public string from { get; set; }
+        public string body { get; set; }
+        public int weight { get; set; }
+        public int groupid { get; set; }
+        public Group group { get; set; }
+    }
+
+    public class Group
+    {
+        public int groupid { get; set; }
+        public string groupname { get; set; }
+    }
+}
